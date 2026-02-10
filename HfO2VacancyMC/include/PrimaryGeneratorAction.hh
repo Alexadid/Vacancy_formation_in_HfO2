b6c8a1ff090026d303a8b6750f037c1117d0c5c1
alexadid <fedosenko.as@phystech.edu>
2026-02-10 19:49:12 +0300

    Switched to actual electron distribution instead of single point

--- HfO2VacancyMC/include/PrimaryGeneratorAction.hh
+++ HfO2VacancyMC/include/PrimaryGeneratorAction.hh
@@ -1,7 +1,9 @@
 #pragma once
+
 #include "G4VUserPrimaryGeneratorAction.hh"
-#include "G4GenericMessenger.hh"
-#include "G4ParticleGun.hh"
+
+class G4GeneralParticleSource;
+class G4Event;
 
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
 public:
@@ -11,9 +13,5 @@ public:
     void GeneratePrimaries(G4Event* anEvent) override;
 
 private:
-    G4ParticleGun* fGun = nullptr;
-    G4GenericMessenger* fMessenger = nullptr;
-
-    double fEnergyKeV = 10.0;       // 1..30 keV
-    double fBeamSigmaNm = 0.0;      // 0 => pencil beam; set >0 for Gaussian spot
+    G4GeneralParticleSource* fGPS = nullptr;
 };
--- HfO2VacancyMC/src/PrimaryGeneratorAction.cc
+++ HfO2VacancyMC/src/PrimaryGeneratorAction.cc
@@ -1,48 +1,17 @@
 #include "PrimaryGeneratorAction.hh"
 
+#include "G4GeneralParticleSource.hh"
 #include "G4Event.hh"
-#include "G4SystemOfUnits.hh"
-#include "G4ParticleTable.hh"
-#include "G4ParticleDefinition.hh"
-#include "G4ThreeVector.hh"
-#include "Randomize.hh"
 
 PrimaryGeneratorAction::PrimaryGeneratorAction() {
-    fGun = new G4ParticleGun(1);
-
-    auto electron = G4ParticleTable::GetParticleTable()->FindParticle("e-");
-    fGun->SetParticleDefinition(electron);
-
-    // Direction: along -Z into the stack (vacuum -> HfO2 -> Si)
-    fGun->SetParticleMomentumDirection(G4ThreeVector(0,0,-1));
-
-    // Default energy
-    fGun->SetParticleEnergy(fEnergyKeV * keV);
-
-    // Start position slightly above top surface (z=0 is top of HfO2)
-    fGun->SetParticlePosition(G4ThreeVector(0,0, +50*nm));
-
-    fMessenger = new G4GenericMessenger(this, "/gun/", "Gun control");
-    fMessenger->DeclareProperty("energyKeV", fEnergyKeV, "Primary electron energy in keV");
-    fMessenger->DeclareProperty("sigmaNm", fBeamSigmaNm, "Gaussian beam sigma in nm (0 => pencil)");
+    fGPS = new G4GeneralParticleSource();
 }
 
 PrimaryGeneratorAction::~PrimaryGeneratorAction() {
-    delete fGun;
-    delete fMessenger;
+    delete fGPS;
 }
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
-    fGun->SetParticleEnergy(fEnergyKeV * keV);
-
-    // Beam spot (optional Gaussian)
-    double x = 0.0, y = 0.0;
-    if (fBeamSigmaNm > 0.0) {
-        const double sigma = fBeamSigmaNm * nm;
-        x = G4RandGauss::shoot(0.0, sigma);
-        y = G4RandGauss::shoot(0.0, sigma);
-    }
-    fGun->SetParticlePosition(G4ThreeVector(x, y, +50*nm));
-
-    fGun->GeneratePrimaryVertex(anEvent);
+    // Generates one primary vertex according to current GPS settings
+    fGPS->GeneratePrimaryVertex(anEvent);
 }
